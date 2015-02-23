8ac0be06f4fbc02183cd92cc40181e836ee510ea
juliocffc <juliocffc@gmail.com>
2015-02-23 12:17:08 -0300

    Implementada a deleção na aplicação

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,14 +9,27 @@ alunosApp.controller('AlunosCtrl',['$scope', '$http',
 	  });
 	  $scope.orderProp = 'contato';
 	$scope.addRowAsyncAsJSON = function(){		
-		$scope.alunos.push({ 'nome':$scope.nome, 'contato': $scope.contato});
+		//$scope.alunos.push({ 'nome':$scope.nome, 'contato': $scope.contato});
 		var dataObj = {
 				nome : $scope.nome,
 				contato : $scope.contato
 		};	
 		var res = $http.post('http://localhost:8090/teste/aluno', dataObj);
 		res.success(function(data) {
-			//$scope.message = data;
+			scope.alunos.push(data);
+		});
+		res.error(function(data) {
+			alert( "failure message: " + JSON.stringify({data: data}));
+		});		
+		$scope.nome='';
+		$scope.contato='';
+	};
+	$scope.deleteRowAsyncAsJSON = function(){		
+		//$scope.alunos.push({ 'nome':$scope.nome, 'contato': $scope.contato});
+	
+		var res = $http.delete('http://localhost:8090/teste/aluno/'+$scope.ra);
+		res.success(function(data) {
+			scope.alunos.push(data);
 		});
 		res.error(function(data) {
 			alert( "failure message: " + JSON.stringify({data: data}));
